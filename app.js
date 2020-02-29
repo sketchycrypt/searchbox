d8534f49ea5c1a5fd1ba2c49d870cb7c5a7e80a6
sketchycrypt <51757052+sketchycrypt@users.noreply.github.com>
2020-02-29 13:01:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 window.addEventListener('load', () => {
 const button = document.querySelector('.search-btn')
+const userinput = document.getElementById("jsinput").value
+//this puts underscores, doesnt work fyi
+
+
+button.addEventListener('click', function() {
+    window.location.href=`https://duckduckgo.com/?q=${userinput}&t=canonical`  
+    userinput = userinput.replace(/\s/g, '');
+    
+});
 
 })
